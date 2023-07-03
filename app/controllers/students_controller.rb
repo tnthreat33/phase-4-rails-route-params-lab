7cde2970701ffcf771f86c2285885a132dd12e9c
Rachel Hamby <tnthreat33@gmail.com>
2023-07-03 15:02:34 -0400

    commit tree 953cfb912e59c36b7b0f6308b2a0779e88280aa3

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,13 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def show 
+    student = Student.find(params[:id])
+    if student
+      render json: student
+    else
+      render json: {error: "user not found"}, status: :not_found
+    end
+  end 
+
 end
